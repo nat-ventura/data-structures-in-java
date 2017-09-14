584afc96fd7c93dfa5cffb824790619ca819b908
Natalie Villasana <navillasa@gmail.com>
2017-09-14 16:58:32 -0400

    begins Maze class and adds 2d array of squares as class variable

---
+++ lab3/Maze.java
@@ -0,0 +1,12 @@
+/**
+ * @author natventura
+ *
+ */
+public class Maze {
+    private int numCols;
+    private int numRows;
+    private Square[][] mazeArray;
+    private Square startSquare;
+    private Square finishSquare;
+    public boolean solvable = true;
+}
