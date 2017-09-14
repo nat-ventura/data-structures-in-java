0daed4ebe4462a7e484adb9a0c66df38591da07c
Natalie Villasana <navillasa@gmail.com>
2017-09-14 15:43:31 -0400

    adds tests for set, add, clear, and isEmpty

--- lab2/MyArrayListTest.java
+++ lab2/MyArrayListTest.java
@@ -111,4 +111,93 @@ public class MyArrayListTest {
 	test.get(-1);
     }
 
+    @Ignore
+    public void testRemove() throws FileNotFoundException {
+	Scanner c = new Scanner(new File("test1.txt"));
+	MyArrayList<String> test = new MyArrayList<String>();
+	MyArrayList<String> second = new MyArrayList<String>();
+	ArrayList<String> real = new ArrayList<String>();
+	ArrayList<String> real2 = new ArrayList<String>();
+
+	while (c.hasNext()) {
+	    String s = c.next();
+	    test.add(s);
+	    real.add(s);
+	}
+	for (int i = 0; i < test.size(); i++) {
+	    if (i % 2 == 0) {
+		second.add(test.remove(i));
+		real2.add(real.remove(i));
+	    }
+	}
+	for (int i = 0; i < second.size(); i++) {
+	    assertSame("Same string?", real2.get(i), second.get(i));
+	}
+	System.out.println(test);
+	System.out.println(second);
+	System.out.println(real);
+	System.out.println(real2);
+    }
+
+    @Ignore
+    public void testSet() throws FileNotFoundException {
+	Scanner c = new Scanner(new File("test1.txt"));
+	MyArrayList<String> test = new MyArrayList<String>();
+	ArrayList<String> real = new ArrayList<String>();
+	ArrayList<String> real2 = real;
+	MyArrayList<String> test2 = test;
+
+	while (c.hasNext()) {
+	    String s = c.next();
+	    test.add(s);
+	    real.add(s);
+	}
+	for (int i = 0; i < test.size(); i++) {
+	    String q = test.get(test.size() - i - 1);
+	    String r = real.get(real.size() - i - 1);
+	    test2.set(i, q);
+	    real2.set(i, r);
+	}
+
+	for (int i = 0; i < test2.size(); i++) {
+	    assertSame("Same string?", real.get(i), test2.get(i));
+	}
+    }
+
+    @Ignore
+    public void testAdd() throws Exception {
+	MyArrayList<Integer> test = new MyArrayList();
+	ArrayList<Integer> real = new ArrayList<Integer>();
+	test.add(1);
+	real.add(1);
+	test.add(2);
+	real.add(2);
+	test.add(0, 0);
+	real.add(0, 0);
+	assertEquals("Size after construction: ", test.size(), real.size());
+    }
+
+    @Ignore
+    public void isEmptyTest() {
+	MyArrayList<String> test = new MyArrayList<String>();
+	assertTrue(test.isEmpty());
+    }
+
+    @Ignore
+    public void clearTest() throws FileNotFoundException {
+	Scanner c = new Scanner(new File("test1.txt"));
+	MyArrayList<String> test = new MyArrayList<String>();
+	ArrayList<String> real = new ArrayList<String>();
+	while (c.hasNext()) {
+	    String s = c.next();
+	    test.add(s);
+	    real.add(s);
+	}
+	test.clear();
+	real.clear();
+	assertEquals(true, test.isEmpty());
+	assertEquals(true, real.isEmpty());
+    }
+
+
 }
