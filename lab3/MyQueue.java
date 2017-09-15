5a13cbd1303c1a194f20deab2f3228f3bfa069ec
Natalie Villasana <navillasa@gmail.com>
2017-09-15 15:50:34 -0400

    adds MyQueue methods

--- lab3/MyQueue.java
+++ lab3/MyQueue.java
@@ -1,34 +1,64 @@
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
 /**
  * @author natventura
  *
+ *
  */
+public class MyQueue<T> implements QueueADT {
+    public ArrayList<T> queue;
 
-public class MyQueue {
-    public class MyQueue<T> implements QueueADT {
-	public ArrayList<T> queue;
+    @SuppressWarnings("unchecked")
+    public MyQueue() {
+	queue = new ArrayList<T>();
+    }
 
-	@SuppressWarnings("unchecked")
-	public MyQueue() {
-	    queue = new ArrayList<T>();
-	}
+    @Override
+    public void enqueue(Object item) {
+	queue.add((T) item);
+    }
 
-	@Override
-	public void enqueue(Object item) {
-	    queue.add((T) item);
-	}
-
-	@Override
-	public T dequeue() {
-	    try {
-		if (queue.size() != 0) {
-		    return queue.remove(0);
-		} else {
-		    throw new NoSuchElementException();
-		}
-	    } catch (NoSuchElementException e) {
-		System.out.println("Queue is empty.");
+    @Override
+    public T dequeue() {
+	try {
+	    if (queue.size() != 0) {
+		return queue.remove(0);
+	    } else {
+		throw new NoSuchElementException();
 	    }
-	    return null;
+	} catch (NoSuchElementException e) {
+	    System.out.println("Queue is empty.");
 	}
+	return null;
+    }
+
+    @Override
+    public T front() {
+	try {
+	    if (queue.size() != 0) {
+		return queue.get(0);
+	    } else {
+		throw new NoSuchElementException();
+	    }
+	} catch (NoSuchElementException e) {
+	    System.out.println("Queue is empty.");
+	}
+	return null;
+    }
+
+    @Override
+    public int size() {
+	return queue.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+	return queue.isEmpty();
+    }
+
+    @Override
+    public void clear() {
+	queue.clear();
     }
 }
