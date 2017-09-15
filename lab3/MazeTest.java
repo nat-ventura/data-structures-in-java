63bf3c1a43920a55805c5b338ca9d880c7e0ef22
Natalie Villasana <navillasa@gmail.com>
2017-09-15 12:36:06 -0400

    adds incomplete junit test class

---
+++ lab3/MazeTest.java
@@ -0,0 +1,42 @@
+//import static org.junit.Assert.*;
+//import org.junit.Test;
+//
+///**
+// * @author natventura
+// *
+// */
+//
+//public class MazeTest {
+//
+//    @Test
+//    public void testLoadMaze() throws Exception {
+//	Maze testMaze = new Maze();
+//	assertTrue("It's working", testMaze.loadMaze("maze - 1"));
+//    }
+//
+//    @Test
+//    public void testGetNeighbors() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void testGetStart() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void testGetFinish() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void testReset() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void testToString() throws Exception {
+//
+//    }
+//
+//}
